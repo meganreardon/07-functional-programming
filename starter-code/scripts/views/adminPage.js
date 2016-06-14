0084adccc9d32a022ef8a15d0ada93ce826ebe74
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-06-14 15:06:05 -0700

    changed variable name to reflect new articles variable

--- starter-code/scripts/views/adminPage.js
+++ starter-code/scripts/views/adminPage.js
@@ -4,7 +4,7 @@ articleView.initAdminPage = function() {
   Article.numWordsByAuthor().forEach(function(stat) {
     $('.author-stats').append(template(stat));
   });
-  $('#blog-stats .articles').text(Article.all.length);
+  $('#blog-stats .articles').text(Article.allArticles.length);
   $('#blog-stats .words').text(Article.numWordsAll());
 };
 
